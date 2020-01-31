b2322469ea4ee92dd266bee5c9dc78a74a41f4f7
noobjc <noobjc@c240g5-110213.wisc.cloudlab.us>
2020-01-31 04:01:28 -0600

    Add data cleanup code in single_stage_detector

--- single_stage_detector/scripts/download_dataset.sh
+++ single_stage_detector/scripts/download_dataset.sh
@@ -5,6 +5,7 @@ set -ex
 apt-get update
 apt-get -y install curl unzip
 
+rm -rf ./single_stage_detector/data
 mkdir -p ./single_stage_detector/data/coco; cd ./single_stage_detector/data/coco
 curl -O http://images.cocodataset.org/zips/train2017.zip; unzip train2017.zip
 curl -O http://images.cocodataset.org/zips/val2017.zip; unzip val2017.zip
