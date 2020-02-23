dd70055ac4e00306fe6d8fed3f053fbea3f56a36
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-02-23 16:47:23 +0530

    Add verify datasets for object detection and single stage detector

---
+++ object_detection/scripts/verify_dataset.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -ex
+
+cd ./object_detection/data/coco
+
+ACTUAL_TEST=`coco_annotations_minival.tgz | md5sum`
+EXPECTED_TEST='2d2b9d2283adb5e3b8d25eec88e65064  -'
+if [[ $ACTUAL_TEST = $EXPECTED_TEST ]]; then
+  echo "OK: correct coco_annotations_minival.tgz"
+else
+  echo "ERROR: incorrect coco_annotations_minival.tgz"
+  echo "ERROR: expected $EXPECTED_TEST"
+  echo "ERROR: found $ACTUAL_TEST"
+fi
+ACTUAL_TEST=`cat annotations_trainval2017.zip | md5sum`
+EXPECTED_TEST='f4bbac642086de4f52a3fdda2de5fa2c  -'
+if [[ $ACTUAL_TEST = $EXPECTED_TEST ]]; then
+  echo "OK: correct annotations_trainval2017.zip"
+else
+  echo "ERROR: incorrect annotations_trainval2017.zip"
+  echo "ERROR: expected $EXPECTED_TEST"
+  echo "ERROR: found $ACTUAL_TEST"
+fi
+ACTUAL_TEST=`cat train2017.zip | md5sum`
+EXPECTED_TEST='cced6f7f71b7629ddf16f17bbcfab6b2  -'
+if [[ $ACTUAL_TEST = $EXPECTED_TEST ]]; then
+  echo "OK: correct train2017.zip"
+else
+  echo "ERROR: incorrect train2017.zip"
+  echo "ERROR: expected $EXPECTED_TEST"
+  echo "ERROR: found $ACTUAL_TEST"
+fi
+ACTUAL_TEST=`cat val2017.zip | md5sum`
+EXPECTED_TEST='442b8da7639aecaf257c1dceb8ba8c80  -'
+if [[ $ACTUAL_TEST = $EXPECTED_TEST ]]; then
+  echo "OK: correct val2017.zip"
+else
+  echo "ERROR: incorrect val2017.zip"
+  echo "ERROR: expected $EXPECTED_TEST"
+  echo "ERROR: found $ACTUAL_TEST"
+fi
