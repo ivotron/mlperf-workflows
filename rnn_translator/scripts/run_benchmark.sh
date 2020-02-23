4916699ccca39036428487a0ca70ec6f5888fbb5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-02-23 11:46:51 +0530

    Rename output dir to results

--- rnn_translator/scripts/run_benchmark.sh
+++ rnn_translator/scripts/run_benchmark.sh
@@ -2,20 +2,20 @@
 set -ex
 
 # Generate the output directory
-mkdir -p ./output/results/gnmt
-mkdir -p ./output/systems
+mkdir -p ./results/gnmt
+mkdir -p ./results/systems
 
 apt-get update
 apt-get install -y facter
 
 # Save facter output
-facter --json >  ./output/systems/system_details.json
+facter --json >  ./results/systems/system_details.json
 
 # Run the training 5 times
 counter=1
 while [ $counter -le 5 ]
 do
-export COMPLIANCE_FILE="/results/gnmt/result_${counter}.txt"
+export COMPLIANCE_FILE="/gnmt/result_${counter}.txt"
 ./rnn_translator/pytorch/run_and_time.sh 1
 ((counter++))
 done
--- single_stage_detector/scripts/run_benchmark.sh
+++ single_stage_detector/scripts/run_benchmark.sh
@@ -2,20 +2,20 @@
 set -ex
 
 # Generate the output directory
-mkdir -p ./output/results/ssd
-mkdir -p ./output/systems
+mkdir -p ./results/ssd
+mkdir -p ./results/systems
 
 apt-get update
 apt-get install -y facter
 
 # Save facter output
-facter --json >  ./output/systems/system_details.json
+facter --json >  ./results/systems/system_details.json
 
 # Run the training 5 times
 counter=1
 while [ $counter -le 5 ]
 do
-export COMPLIANCE_FILE="/results/ssd/result_${counter}.txt"
+export COMPLIANCE_FILE="/ssd/result_${counter}.txt"
 ./single_stage_detector/ssd/run_and_time.sh
 ((counter++))
 done
