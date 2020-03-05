03f45c9d61b3dfbd32663d6f721f1712d7788bb2
noobjc <noobjc@c240g5-110125.wisc.cloudlab.us>
2020-03-05 12:20:27 -0600

    Separated download and extract step for ssd

--- single_stage_detector/scripts/collect_system_info.sh
+++ single_stage_detector/scripts/collect_system_info.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 
 mkdir -p ./results/closed/systems
--- single_stage_detector/scripts/download_dataset.sh
+++ single_stage_detector/scripts/download_dataset.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 
 # Get COCO 2017 data sets
-
 mkdir -p ./data/coco
 cd ./data/coco
 
@@ -10,19 +9,16 @@ if [ ! -d train2017 ]; then
 if [ ! -f train2017.zip ]; then
 curl -O http://images.cocodataset.org/zips/train2017.zip
 fi
-unzip -n train2017.zip
 fi
 
 if [ ! -d val2017 ]; then
 if [ ! -f val2017.zip ]; then
 curl -O http://images.cocodataset.org/zips/val2017.zip
 fi
-unzip -n val2017.zip
 fi
 
 if [ ! -d annotations ]; then
 if [ ! -f annotations_trainval2017.zip ]; then
 curl -O http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 fi
-unzip -n annotations_trainval2017
 fi
