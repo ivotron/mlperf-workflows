e75f80f2415f61ea61535edb50c8c6798d8dc97f
noobjc <noobjc@c240g5-110125.wisc.cloudlab.us>
2020-03-07 02:47:33 -0600

    remove useless ret_code statements

--- object_detection/pytorch/run_and_time.sh
+++ object_detection/pytorch/run_and_time.sh
@@ -13,10 +13,8 @@ start_fmt=$(date +%Y-%m-%d\ %r)
 echo "STARTING TIMING RUN AT $start_fmt"
 
 # Single GPU training
-time python tools/train_mlperf.py --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
+python tools/train_mlperf.py --config-file "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
        SOLVER.IMS_PER_BATCH 2 TEST.IMS_PER_BATCH 1 SOLVER.MAX_ITER 7500 SOLVER.STEPS "(3750, 5000)" SOLVER.BASE_LR 0.02
-
-if [[ $ret_code != 0 ]]; then exit $ret_code; fi
 
 # end timing
 end=$(date +%s)
